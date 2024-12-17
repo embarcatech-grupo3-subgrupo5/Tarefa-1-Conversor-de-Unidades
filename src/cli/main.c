02a82b330a6998fefc116c85b9bfebbcbb914fc3
Geison de Oliveira <geisonyt3@gmail.com>
2024-12-17 17:43:11 -0300

    Configuração inicial do projeto

---
+++ src/cli/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bem-vindo ao Conversor de Unidades!\n");
+    printf("Escolha uma opção para continuar:\n");
+    printf("1. Comprimento\n");
+    printf("2. Massa\n");
+    printf("3. Temperatura\n");
+    return 0;
+}
