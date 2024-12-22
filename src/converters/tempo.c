3f4e9911b682f5c76e6174a45cbfc4f95a17efaa
Mario Vinicius <marioviniciuxcs@gmail.com>
2024-12-21 21:38:38 -0300

    feat: adição da funcionalidade de conversão de tempo

---
+++ src/converters/tempo.c
@@ -0,0 +1,32 @@
+#include <string.h>
+
+// os valores que podem ser passados na origem e destino são: ms, s, min, h, d
+double converter_tempo(double valor, char unidade_origem[], char unidade_destino[]) {
+    double valor_convertido = valor;
+
+    if (strcmp(unidade_origem, "ms") == 0) {
+        valor_convertido = valor / 1000; 
+    } else if (strcmp(unidade_origem, "s") == 0) {
+        valor_convertido = valor;
+    } else if (strcmp(unidade_origem, "min") == 0) {
+        valor_convertido = valor * 60;
+    } else if (strcmp(unidade_origem, "h") == 0) {
+        valor_convertido = valor * 3600;
+    } else if (strcmp(unidade_origem, "d") == 0) {
+        valor_convertido = valor * 86400; 
+    }
+
+    if (strcmp(unidade_destino, "ms") == 0) {
+        valor_convertido = valor_convertido * 1000; 
+    } else if (strcmp(unidade_destino, "s") == 0) {
+        valor_convertido = valor_convertido; 
+    } else if (strcmp(unidade_destino, "min") == 0) {
+        valor_convertido = valor_convertido / 60; 
+    } else if (strcmp(unidade_destino, "h") == 0) {
+        valor_convertido = valor_convertido / 3600;
+    } else if (strcmp(unidade_destino, "d") == 0) {
+        valor_convertido = valor_convertido / 86400; 
+    }
+
+    return valor_convertido;
+}
