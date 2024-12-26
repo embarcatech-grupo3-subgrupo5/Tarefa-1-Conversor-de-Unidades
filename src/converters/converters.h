a23b0cabeefd9ea1b85c7d05a7f84285672d0db9
Julierme <julicomjesus@gmail.com>
2024-12-26 10:57:37 -0300

    implementação da biblioteca de conversão de massa

--- src/converters/converters.h
+++ src/converters/converters.h
@@ -33,3 +33,41 @@ double kelvin_para_celsius(double kelvin);
  * @param kelvin Entrada com a temperatura em kelvin
  */
 double kelvin_para_fahrenheit(double kelvin);
+
+////////////////////////////////////////////////////
+
+/**
+ * @brief Grama para Kilograma
+ * @param grama Entrada com a massa em gramas
+ */
+double grama_para_kilograma(double grama);
+
+/**
+ * @brief Kilograma para Grama
+ * @param kilograma Entrada com a massa em kilogramas
+ */
+double kilograma_para_grama(double kilograma);
+
+/**
+ * @brief Kilograma para Tonelada
+ * @param kilograma Entrada com a massa em kilograma
+ */
+double kilograma_para_tonelada(double kilograma);
+
+/**
+ * @brief Tonelada para Kilograma
+ * @param tonelada Entrada com a massa em tonelada
+ */
+double tonelada_para_kilograma(double tonelada);
+
+/**
+ * @brief Grama para Tonelada
+ * @param grama Entrada com a massa em grama
+ */
+double grama_para_tonelada(double grama);
+
+/**
+ * @brief Tonelada para Grama
+ * @param tonelada Entrada com a massa em tonelada
+ */
+double tonela_para_grama(double tonelada);
